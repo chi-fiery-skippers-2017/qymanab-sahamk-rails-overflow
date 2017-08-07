ffa5b9b03db63ae2a2e5399e314a4bb4ad630331
Qymana Botts & Saham Khozestani <pair+qymanab.sahamak@devbootcamp.com>
2017-08-07 15:51:36 -0500

    Add session helpers, add user model/migration, update associations accordingly

---
+++ db/migrate/20170807204326_add_best_answer_column_in_questions.rb
@@ -0,0 +1,5 @@
+class AddBestAnswerColumnInQuestions < ActiveRecord::Migration[5.1]
+  def change
+    add_column :questions, :best_answer_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807163909) do
+ActiveRecord::Schema.define(version: 20170807204326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20170807163909) do
     t.string "title", null: false
     t.string "body", null: false
     t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "best_answer_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
