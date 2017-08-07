da0c9c042baf50513aa477c20d641c2e6e1b980f
Qymana Botts & Saham Khozestani <pair+qymanab.sahamak@devbootcamp.com>
2017-08-07 11:27:05 -0500

    create new question route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,20 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :body)
+    end
+
 end
